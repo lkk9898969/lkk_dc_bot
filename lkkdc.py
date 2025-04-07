7257ea8c0cb2e97f6d6ca9a3c9181971134535c1
B11000002 <B11000002@live.yuntech.edu.tw>
2025-04-07 20:39:57 +0800

    add CLI input command support

--- lkkdc.py
+++ lkkdc.py
@@ -1,10 +1,11 @@
 import discord, asyncio, json, time
+from aioconsole import ainput as input_async
 from pathlib import Path
 from discord.ext import commands
 from core.lkk_log import loggerhandler
 
 cogs = "cogs"
-botjson = 'lkk.json'
+botjson = "lkk.json"
 
 
 class lkkdc(commands.Bot):
@@ -12,11 +13,10 @@ class lkkdc(commands.Bot):
     def __init__(self, owner_id: int):
         intents = discord.Intents.default()
         intents.message_content = True
-        activity = discord.Activity(type=discord.ActivityType.custom,
-                                    name="custom",state="打/看指令表")
-        super().__init__(command_prefix="$",
-                         intents=intents,
-                         activity=activity)
+        activity = discord.Activity(
+            type=discord.ActivityType.custom, name="custom", state="打/看指令表"
+        )
+        super().__init__(command_prefix="$", intents=intents, activity=activity)
         self.owner_id = owner_id
 
     async def setup_hook(self) -> None:
@@ -40,19 +40,37 @@ class lkkdc(commands.Bot):
 def init():
     global logger, data
     logname = "lkkdc"
-    logger = loggerhandler("",
-                           logname + time.strftime("-%Y%m%d"),
-                           cwd="log",
-                           logfile_level="DEBUG")
+    logger = loggerhandler(
+        "", logname + time.strftime("-%Y%m%d"), cwd="log", logfile_level="DEBUG"
+    )
 
-    with open(botjson, 'r', encoding="utf-8") as file:
+    with open(botjson, "r", encoding="utf-8") as file:
         data = json.load(file)
     logger.info(f"載入資料成功，持有者ID={data['OWNER_ID']}")
 
 
+async def clicmd_list(cmd: str, **kwargs):
+    match cmd.lower():
+        case "stop" | "exit":
+            logger.info("Stop bot.")
+            await kwargs["bot"].close()
+            exit(0)
+
+
 async def main():
-    bot = lkkdc(int(data['OWNER_ID']))
-    await bot.start(data["TOKEN"])
+    bot = lkkdc(int(data["OWNER_ID"]))
+    asyncio.create_task(bot.start(data["TOKEN"]))
+    while True:
+        inputs = str(await input_async())
+        logger.info(inputs)
+        logger.debug(inputs.encode())
+        cmd = bot.get_command(inputs)
+        if cmd:
+            await bot.invoke(cmd)
+        elif await clicmd_list(inputs, bot=bot):
+            pass
+        else:
+            logger.error(f"{inputs} command not found!")
 
 
 # 確定執行此py檔才會執行
